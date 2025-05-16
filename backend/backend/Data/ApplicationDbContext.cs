156070cf919c9c21146e1803f42c39b9f659aa5d
Mohammad T Hossein <56173085+hossem7@users.noreply.github.com>
2025-05-15 21:11:10 -0400

    ignore some files and cleanup project

---
+++ backend/backend/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<TaskItem> Tasks { get; set; }
+    }
+}
---
+++ backend/backend/Models/TaskItem.cs
@@ -0,0 +1,10 @@
+﻿namespace backend.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; } // primary key
+        public string Title { get; set; } // task title
+        public bool IsCompleted { get; set; } // status flag
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
